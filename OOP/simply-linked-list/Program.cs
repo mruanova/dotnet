98adf9c42e134ca53af12c5ab6751acf8b97f135
Mauricio Ruanova-Hurtado <mruanova@gmail.com>
2020-06-08 11:37:01 -0500

    data

---
+++ OOP/simply-linked-list/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simply_linked_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
